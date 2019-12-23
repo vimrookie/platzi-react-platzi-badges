8c413dd3b68b3e48aa15093853bf06deac31c657
Gleycer Parra <gleycer1@gmail.com>
2019-12-23 13:44:37 -0500

    Updates

---
+++ precache-manifest.5a741978a7991f6f3cbc6e5800f1768c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3407c5d7ebb7542a11633ab58c873b56",
+    "url": "/react-platzi-badges/index.html"
+  },
+  {
+    "revision": "60b9b0729d95d324b1a4",
+    "url": "/react-platzi-badges/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "9a5744808e93b8db5c24",
+    "url": "/react-platzi-badges/static/css/main.eddca697.chunk.css"
+  },
+  {
+    "revision": "60b9b0729d95d324b1a4",
+    "url": "/react-platzi-badges/static/js/2.6e1cb7c0.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/react-platzi-badges/static/js/2.6e1cb7c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a5744808e93b8db5c24",
+    "url": "/react-platzi-badges/static/js/main.290ce8cb.chunk.js"
+  },
+  {
+    "revision": "abf8185e96193496c575",
+    "url": "/react-platzi-badges/static/js/runtime-main.a0ad5015.js"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/react-platzi-badges/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/react-platzi-badges/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/react-platzi-badges/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/react-platzi-badges/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/react-platzi-badges/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "182d5ef4d173a3cd93897242a6fd56a5",
+    "url": "/react-platzi-badges/static/media/twitter.182d5ef4.svg"
+  }
+]);
